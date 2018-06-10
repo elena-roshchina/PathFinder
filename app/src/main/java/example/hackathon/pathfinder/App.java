eb454b1133b212ba335cf798e5c4e85fc92a6eb1
elena-roshchina <star-fox@yandex.ru>
2018-06-10 16:07:02 +0300

    ApiFactory -> App

--- app/src/main/java/example/hackathon/pathfinder/App.java
+++ app/src/main/java/example/hackathon/pathfinder/App.java
@@ -6,7 +6,7 @@ import example.hackathon.pathfinder.api.AirbnbApi;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
-public class ApiFactory extends Application {
+public class App extends Application {
     private static AirbnbApi airbnbApi;
     private static final String AIRBNB_BASE_URL = "https://api.airbnb.com";
     private static final String AIRBNB_CLIENT_ID = "3092nxybyb0otqw18e8nh5nty";
