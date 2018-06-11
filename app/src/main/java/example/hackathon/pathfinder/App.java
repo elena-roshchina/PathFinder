58da4b55cc7ad0483358fe2b5b1cc68656e5bacc
elena-roshchina <star-fox@yandex.ru>
2018-06-11 03:30:54 +0300

    TRAVELPAYOUTS_BASE_URL removed from App.

--- app/src/main/java/example/hackathon/pathfinder/App.java
+++ app/src/main/java/example/hackathon/pathfinder/App.java
@@ -12,7 +12,6 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 public class App extends Application {
 
-    private static final String TRAVELPAYOUTS_BASE_URL = "http://api.travelpayouts.com/v2/";
     private static final String TRAVELPAYOUTS_CLIENT_ID = "0e29a686b2ad016b6d43087b0f441dbe";
 
     public static AirbnbApi getAirbnbApi() {
