e53a5dadb112e307f098ece2857f2b221438f734
Alex <urumtseval@gmail.com>
2018-06-11 11:32:22 +0300

    final proper lining

--- app/src/main/java/example/hackathon/pathfinder/FlightsAdapter.java
+++ app/src/main/java/example/hackathon/pathfinder/FlightsAdapter.java
@@ -43,7 +43,7 @@ public class FlightsAdapter extends RecyclerView.Adapter<FlightsAdapter.FlightsV
     public void onBindViewHolder(@NonNull FlightsViewHolder holder, int position) {
         final Flight flight = flights.get(position);
 
-        Integer flightPrice = flight.getValue();
+        Integer flightPrice = Math.round(flight.getValue()/62);
         String startDate = flight.getDepartDate();
         String endDate = flight.getReturnDate();
         Integer classType = flight.getTripClass();
@@ -78,7 +78,7 @@ public class FlightsAdapter extends RecyclerView.Adapter<FlightsAdapter.FlightsV
                 public void onClick(View v) {
                     int idOnClick = getAdapterPosition();
                     Flight flight = flights.get(idOnClick);
-                    int price = Math.round(((userInfo.getSum() - flight.getValue()) / (userInfo.getDuration()*7)) / 70);
+                    int price = ((userInfo.getSum() - flight.getValue()) / (userInfo.getDuration()*7));
                     v.getContext().startActivity(
                             Apartments.apptsIntent(
                                     v.getContext(),
