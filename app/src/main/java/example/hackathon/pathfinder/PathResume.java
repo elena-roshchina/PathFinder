98484941189a2bcec497f8e69af2c1c8ecd3e4e9
elena-roshchina <star-fox@yandex.ru>
2018-06-11 06:40:29 +0300

    Layout for last activity (PathResume).

--- app/src/main/java/example/hackathon/pathfinder/PathResume.java
+++ app/src/main/java/example/hackathon/pathfinder/PathResume.java
@@ -1,7 +1,12 @@
 package example.hackathon.pathfinder;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
 public class PathResume extends AppCompatActivity {
-
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
 }
